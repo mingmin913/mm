a3026e49d353daa19ed3d876e1695587e3b985b9
mingmin913 <mfnbshje@gmail.com>
2026-02-26 17:32:11 +0900

    260226 을유

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -193,4 +193,34 @@ $(document).ready(function(){
             },
          
         });
+        /******************************
+         * story_swiper
+         * ********************************/
+        const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            579: {   
+                slidesPerView: 2,       
+                spaceBetween: 24,
+            },
+            769: {    
+                slidesPerView: 3,   
+                spaceBetween: 24,
+            },
+            1025: {    /* 640px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+       
+        navigation: {
+            nextEl: '.story .next',
+            prevEl: '.story .prev',
+        },
+      
+    });
+     
 })
